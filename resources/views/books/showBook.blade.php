2d795089ab563a8ae9009c8e72553356d73f63fc
Dolores Haze <mariana.eli.corona@gmail.com>
2022-04-05 17:03:28 -0500

    Mostrar

--- resources/views/books/showBook.blade.php
+++ resources/views/books/showBook.blade.php
@@ -7,27 +7,51 @@
     <title>Libro</title>
 </head>
 <body>
-    <h1>Informacion del Libro</h1>
-    <table> 
-        <tr>
-            <th>ID</th>
-            <th>Titulo</th>
-            <th>Autor</th>
-            <th>Editorial</th>
-            <th>Año</th>
-            <th>Lenguaje</th>
-        </tr>
-        
-        <tr>
-            <td>{{$book->id}}</td>
-            <td>{{$book->title}}</td>
-            <td>{{$book->author}}</td>
-            <td>{{$book->editorial}}</td>
-            <td>{{$book->year}}</td>
-            <td>{{$book->language}}</td>
-        </tr>
-        
-    </table>
+    <x-layout>
+      <!-- page content -->
+      <div class="right_col" role="main">
+        <div class="">
+          <div class="page-title">
+            <div class="title_left">
+                <h1>Informacion del Libro</h1>
+            </div>
+
+            <div class="title_right">
+                <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+                  <div class="input-group">
+                    <span class="input-group-btn">
+                      <a class="btn btn-dark mt-3" href="/books">Regresar</a>     
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div> 
+          </div>
     
+          <div class="table-responsive-sm">
+            <table class="table table-hover"> 
+              <thead class="table-dark">
+                <tr>
+                  <th>ID</th>
+                  <th>Titulo</th>
+                  <th>Autor</th>
+                  <th>Editorial</th>
+                  <th>Año</th>
+                  <th>Lenguaje</th>
+                </tr>
+              </thead>
+
+                <tr>
+                  <td>{{$book->id}}</td>
+                  <td>{{$book->title}}</td>
+                  <td>{{$book->author}}</td>
+                  <td>{{$book->editorial}}</td>
+                  <td>{{$book->year}}</td>
+                  <td>{{$book->language}}</td>
+                </tr>
+            </table>
+          </div>
+        </div>
+      </x-layout>
 </body>
 </html>
