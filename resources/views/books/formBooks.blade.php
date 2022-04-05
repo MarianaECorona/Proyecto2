ee56b36f9a409c4cb096891b67844910713b207e
Dolores Haze <mariana.eli.corona@gmail.com>
2022-04-05 15:13:46 -0500

    Formularios

--- resources/views/books/formBooks.blade.php
+++ resources/views/books/formBooks.blade.php
@@ -5,69 +5,94 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
 </head>
 <body>
-    <h1>Agregar Libros</h1>
+    
+    @isset($book) 
 
-    @isset($book)
-        <form action="/books/{{$book->id}}" method="POST" enctype = "multipart/form-data"> {{-- editar --}}
-        @method('PATCH') 
+        <div class="container text-center mt-5">
+            <h1>Editar Libro</h1>
+        </div>
+        <section class="d-flex justify-content-center">
+            <div class="col-md-2 mt-5">
+                <div class="mb-3 mt-3">
+                    <!--INICIO FORMULARIO-->
+                    <form action="/books/{{$book->id}}" method="POST" enctype = "multipart/form-data"> {{-- editar --}}
+                     @method('PATCH') 
+
     @else
-        <form action="/books" method="POST" enctype = "multipart/form-data"> {{-- Crear --}}
+
+        <div class="container text-center mt-5">
+            <h1>Agregar Libros</h1>
+        </div>
+        <section class="d-flex justify-content-center">
+            <div class="col-md-2 mt-5">
+                <div class="mb-3 mt-3">
+                    <!--INICIO FORMULARIO-->
+                    <form action="/books" method="POST" enctype = "multipart/form-data"> {{-- Crear --}}
     @endisset
 
-        @csrf
+                        @csrf
 
-        <label for="title">Titulo</label><br>
-        <input type="text" name="title" value="{{ isset($book) ? $book->title : '' }} {{old('title')}} "><br>
+                        <label for="title">Titulo</label><br>
+                        <input type="text" name="title" value="{{ isset($book) ? $book->title : '' }} {{old('title')}} "><br>
 
-        <br> 
-        @error('title')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        <br>
+                        <br> 
+                        @error('title')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                        <br>
 
-        <label for="author">Autor</label><br>
-        <input type="text" name="author"  value="{{ isset($book) ? $book ->author : ''  }} {{old('author')}}"><br>
+                        <label for="author">Autor</label><br>
+                        <input type="text" name="author"  value="{{ isset($book) ? $book ->author : ''  }} {{old('author')}}"><br>
 
-        <br> 
-        @error('author')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        <br>
+                        <br> 
+                        @error('author')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                        <br>
 
-        <label for="editorial">Editorial</label><br>
-        <input type="text" name="editorial"  value="{{ isset($book) ? $book ->editorial : '' }} {{old('editorial')}}"><br>
+                        <label for="editorial">Editorial</label><br>
+                        <input type="text" name="editorial"  value="{{ isset($book) ? $book ->editorial : '' }} {{old('editorial')}}"><br>
 
-        <br> 
-        @error('editorial')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        <br>
+                        <br> 
+                        @error('editorial')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                        <br>
 
-        <label for="year">Año</label><br>
-        <input type="text" name="year"  value="{{ isset($book) ? $book ->year : ''  }} {{old('year')}}"><br>
+                        <label for="year">Año</label><br>
+                        <input type="text" name="year"  value="{{ isset($book) ? $book ->year : ''  }} {{old('year')}}"><br>
 
-        <br> 
-        @error('year')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        <br>
+                        <br> 
+                        @error('year')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                        <br>
 
-        <label for="language">Idioma</label><br>
-        <select name="language" id="language">
-            <option value="spanish"{{ isset($book) && $book->language == 'spanish' ? 'selected' : '' }}>Español</option>
-            <option value="english"{{ isset($book) && $book->language == 'english' ? 'selected' : '' }}>Ingles</option>
-            <option value="french"{{ isset($book) && $book->language == 'french' ? 'selected' : '' }}>Frances</option>
-        </select><br>
+                        <label for="language">Idioma</label><br>
+                        <select name="language" id="language">
+                            <option value="spanish"{{ isset($book) && $book->language == 'spanish' ? 'selected' : '' }}>Español</option>
+                            <option value="english"{{ isset($book) && $book->language == 'english' ? 'selected' : '' }}>Ingles</option>
+                            <option value="french"{{ isset($book) && $book->language == 'french' ? 'selected' : '' }}>Frances</option>
+                        </select><br>
 
-        <br> 
-        @error('language')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        <br>
+                        <br> 
+                        @error('language')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                        <br>
 
-        <input type="submit" value="Submit">
-    </form>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <input class="btn btn-dark" type="submit" value="Submit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/books/indexBooks.blade.php
+++ resources/views/books/indexBooks.blade.php
@@ -8,65 +8,62 @@
 </head>
 <body>
 
-   <x-layout> 
-    <!-- page content -->
+  <x-layout> 
+  <!-- page content -->
     <div class="right_col" role="main">
-        <div class="">
-          <div class="page-title">
-            <div class="title_left">
-                <h1>Libros</h1>
-            </div>
-            <div class="title_right">
-              <div class="col-md-5 col-sm-5   form-group pull-right top_search">
-                <div class="input-group">
-                  <span class="input-group-btn">
-                    <a class="btn btn-dark mt-3" href="books/create">Añadir Libro</a>     
-                  </span>
-                </div>
-                  <div class="container text-center mt-5">
-                           
-                  </div>
-            </div>
+      <div class="">
+        <div class="page-title">
+          <div class="title_left">
+            <h1>Libros</h1>
           </div>
 
-          
+          <div class="title_right">
+            <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <a class="btn btn-dark mt-3" href="books/create">Añadir Libro</a>     
+                </span>
+              </div>
+            </div>
+          </div>
+        </div> 
+      </div>
 
-  <div class="table-responsive-sm fs-3">
-    <table class="table table-hover"> 
-      <thead class="table-dark">
-        <tr>
-            <th>ID</th>
-            <th>Titulo</th>
-            <th>Autor</th>
-            <th>Editorial</th>
-            <th>Año</th>
-            <th>Lenguaje</th>
-            <th>Acciones</th>
-        </tr>
-
-      </thead>
-        @foreach($books as $book)
+      <div class="table-responsive-sm">
+        <table class="table table-hover"> 
+          <thead class="table-dark">
             <tr>
-                <td>{{$book->id}}</td>
-                <td>{{$book->title}}</td>
-                <td>{{$book->author}}</td>
-                <td>{{$book->editorial}}</td>
-                <td>{{$book->year}}</td>
-                <td>{{$book->language}}</td>
-                <td>
-                    <a href="books/{{$book->id}}">Ver detalle</a> |
-                    <a href="books/{{$book->id}}/edit">Editar</a> <br>
-                    <form action="/books/{{$book->id}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <input class="btn btn-sm btn-dark" type="submit" value="delete">
-                    </form>
-
-                </td>
+              <th>ID</th>
+              <th>Titulo</th>
+              <th>Autor</th>
+              <th>Editorial</th>
+              <th>Año</th>
+              <th>Lenguaje</th>
+              <th>Acciones</th>
             </tr>
-        @endforeach
-    </table>
-  </div>
-   </x-layout>
+          </thead>
+          @foreach($books as $book)
+            <tr>
+              <td>{{$book->id}}</td>
+              <td>{{$book->title}}</td>
+              <td>{{$book->author}}</td>
+              <td>{{$book->editorial}}</td>
+              <td>{{$book->year}}</td>
+              <td>{{$book->language}}</td>
+              <td>
+                <a href="books/{{$book->id}}">Ver detalle</a> |
+                <a href="books/{{$book->id}}/edit">Editar</a> <br>
+                <form action="/books/{{$book->id}}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <input class="btn btn-sm btn-dark" type="submit" value="delete">
+                </form>
+              </td>
+            </tr>
+          @endforeach
+        </table>
+      </div>
+    </div>
+  </x-layout>
 </body>
 </html>
